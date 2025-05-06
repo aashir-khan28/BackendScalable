bf33bf159707d53133f14369741770571c1d50bd
nabin bhattarai <nabin.bhattarai@varosatech.com>
2025-05-06 19:56:56 +0100

    publish node js test 6

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -10,27 +13,22 @@ const authRoutes = require("./src/routes/authRoutes");
 const photoRoutes = require("./src/routes/photoRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.use(helmet());  // Adds security-related HTTP headers
-app.use(compression());  // Compress response bodies for all requests
-app.use(morgan("dev"));  // Log HTTP requests to the console
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
 
 // Middleware
-app.use(cors(
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  preflightContinue: false
+}));
 
-  {
-    origin: "http://localhost:3000",  // Allow requests from this origin
-    methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed HTTP methods
-    credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
-  preflightContinue: false,  // Preflight requests are automatically handled by the browser
-  }
-
-));  // Enable cross-origin requests
-
-// Built-in middleware for parsing JSON and URL-encoded data
-app.use(express.json());  // For parsing application/json
-app.use(express.urlencoded({ extended: true }));  // For parsing application/x-www-form-urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Connect to DB
 connectDB();
@@ -42,7 +40,7 @@ app.get('/', (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", photoRoutes);
 
-// Error handling middleware for catching errors
+// Error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
